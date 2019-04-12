685c2f207d6c4c17a474fdc86639432a30600133
JosefPeck <43961885+JosefPeck@users.noreply.github.com>
2019-04-12 20:27:50 +0000

    color

---
+++ minesweeper_clone/colors.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+
+class Color:
+
+	DARK_GREY: str = '\033[90m'
+	RED: str = '\033[31m'
+	GREEN: str = '\033[32m'
+	BLUE: str = '\033[34m'
+	CYAN: str = '\033[36m'
+	LIGHT_BLUE: str = '\033[94m'
+	LIGHT_GREY: str = '\033[37m'
+	ORANGE: str = '\033[33m'
+	PINK: str = '\033[95m'
+	PURPLE: str = '\033[35m'
+	YELLOW: str = '\033[93m'
+
+	def __init__(self):
+		# TODO: change depending on terminal
+		pass
