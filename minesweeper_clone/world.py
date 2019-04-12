55abfef11c36e17f82c9948b20696f2c4101eef1
JosefPeck <43961885+JosefPeck@users.noreply.github.com>
2019-04-12 20:17:53 +0000

    attack for mines

--- minesweeper_clone/world.py
+++ minesweeper_clone/world.py
@@ -5,6 +5,12 @@ import random
 
 
 class World:
+
+	chara = {
+		'empty': '☐',
+		'mine': '☒'
+	}
+
 	width: int = 0
 	height: int = 0
 	world: list = None
@@ -19,13 +25,13 @@ class World:
 		self.width = width
 		self.height = height
 		self.mines = mines
-		self.world = ['☐'] * (width * height)
+		self.world = [World.chara['empty']] * (width * height)
 		self.generate()
 
 	def generate(self):
 		length = len(self.world)
 		for i in range(0, self.mines):
-			self.world[random.randrange(length)] = '☒'
+			self.world[random.randrange(length)] = World.chara['mine']
 
 	def get(self, x: int, y: int) -> int:
 		return self.world[x + y * self.height]
@@ -37,13 +43,33 @@ class World:
 		"""
 		instr_list = instr.split(';')
 		for cmd in instr_list:
-			cmd = cmd.split(' ')
-			imperat = cmd[0]
-			if imperat == 'q' or imperat == 'quit' or imperat == 'exit':
+			cmd: list = cmd.split(' ')
+			imperat: str = cmd[0]
+			if imperat in {'q', 'e', 'quit', 'exit'}:
 				exit('Bye')
-			elif imperat == 'a' or imperat == 'attack':
-				print('')
-			elif imperat == 's' or imperat == 'see':
+			elif imperat in {'a', 'attack'}:
+				# Check to see if it has an x and y.
+				if len(cmd) != 3:
+					print('?')
+					return
+				x: int = 0
+				y: int = 0
+				try:
+					x = int(cmd[1])
+					y = int(cmd[2])
+				except ValueError:
+					print('?')
+					return
+				if (x < 0) or (y < 0) or (self.width < x) or (self.height < 0):
+					print('?')
+					return
+				cell = self.get(x, y)
+				print('Got: {}'.format(cell))
+				if cell == World.chara['mine']:
+					exit('Game over')
+			elif imperat in {'c', 'check'}:
+				print('999% chance of mine')
+			elif imperat in {'s', 'see'}:
 				print('Seeing map')
 			else:
 				print('?')
