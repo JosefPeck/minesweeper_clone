7f91e3de66f0ee1e9fafb8a4dd542a9e6aa5668b
JosefPeck <43961885+JosefPeck@users.noreply.github.com>
2019-04-12 14:18:09 +0000

    make pylintrc work

--- minesweeper_clone/console.py
+++ minesweeper_clone/console.py
@@ -11,7 +11,7 @@ class Console:
 	def clear_screen(self):
 		os.system('cls' if os.name == 'nt' else 'clear')
 
-	def draw_world(self, world_height: int, world_data: list):
+	def draw_world(self, world_height: int, world_data: list) -> str:
 		final_buffer = []
 		for index, cell in enumerate(world_data):
 			row_buffer = []
@@ -19,4 +19,7 @@ class Console:
 				row_buffer.append('\n')
 			row_buffer.append(str(cell))
 			final_buffer.append(''.join(row_buffer))
-		print(''.join(final_buffer))
+		return ''.join(final_buffer)
+
+	def print_world(self, world_height: int, world_data: list):
+		print(self.draw_screen(world_height, world_data))
